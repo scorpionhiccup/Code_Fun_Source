9166605be631294a5cfc033b28e49fe1a84ac51c
scorpionhiccup <sharvil.2009@gmail.com>
2015-03-29 01:50:20 +0530

    Extracting from API's

---
+++ Code_Fun_Source/CodeForcesUser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Fun_Source
+{
+    public class CodeForcesUser
+    {
+        public string handle { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string country { get; set; }
+        public string city { get; set; }
+        public string organization { get; set; }
+        public int contribution { get; set; }
+        public string rank { get; set; }
+        public int rating { get; set; }
+        public string maxRank { get; set; }
+        public int maxRating { get; set; }
+        public int lastOnlineTimeSeconds { get; set; }
+        public int registrationTimeSeconds { get; set; }
+    }
+
+    public class MainUserObject
+    {
+        public string status { get; set; }
+        public List<CodeForcesUser> result { get; set; }
+    }
+    
+}
